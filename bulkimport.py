b900e8895624e03ded98db7fa78466e8fa919175
Rezagholi Faryan <faryan.rezagholi@siedle.de>
2022-03-14 17:57:06 +0100

    fixed host json

--- bulkimport.py
+++ bulkimport.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#v1.0 Made by Michael Kaplan
-#v1.1 Made by Faryan Rezagholi
+#v1.0 Michael Kaplan
+#v1.1 Faryan Rezagholi
 
 from zabbix_api import ZabbixAPI
 import csv
 from progressbar import ProgressBar, Percentage, ETA, ReverseBar, RotatingMarker, Timer
 
+
 ############################## Define Parameters ##############################
-zapi = ZabbixAPI("https://zabbix/api_jsonrpc.php")
-zapi.login(user="user", password="pass")
-groupid = 25 #edit group id, you can find it using zabbix-cli
-templateid = 11433 #edit templete id you can find it using zabbix-cli
+url = "https://zabbix/api_jsonrpc.php"
+username = "user"
+secret = "pass"
+groupid = 2 # set group id (you can find it using zabbix-cli)
+char = ";" # delimiting character in csv
+path = "C:\\hosts.csv" # change the file name to that one you intend to use 
+port = 161
 ############################ End Define Parameters ############################
 
+
+zapi = ZabbixAPI(url)
 zapi.session.verify = False
-file = csv.reader(open(r"C:\hosts.csv"))
+zapi.login(user=username, password=secret)
+file = csv.reader(open(path))
 lines = sum(1 for line in file)
 bar = ProgressBar(maxval=lines,widgets=[Percentage(), ReverseBar(), ETA(), RotatingMarker(), Timer()]).start()
 i = 0
-f = csv.reader(open(r"C:\hosts.csv"), delimiter=';') #Change the file name to that one you intend to use 
+f = csv.reader(open(path), delimiter=char)
 
-for [hostname,ip] in f: #edit colums to be readed and set variables for parameters
+for [hostname,ip] in f:
     zapi.host.create({
         "host": hostname ,
         "interfaces":[{
-            "type": 2,
+            "type": 1,
             "main": 1,
             "useip": 1,
             "ip": ip,
             "dns": hostname,
-            "port": 161
+            "port": port
         }],
         "groups":[{
             "groupid": groupid
-        }],
-        "templates":[{
-            "templateid": templateid
         }]
         }
     )
